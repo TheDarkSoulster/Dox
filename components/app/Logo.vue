a4f49020692051bd719b23225af328f99a5b8c35
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-15 09:51:35 +0530

    Updated `Logo.vue` as a test with `ID=2`

--- components/app/Logo.vue
+++ components/app/Logo.vue
@@ -7,7 +7,7 @@
     viewBox="0 0 400.000000 100.000000"
     preserveAspectRatio="xMidYMid meet"
   >
-    <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+    <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)" fill="#6965db" stroke="none">
       <path d="M1690 601 l0 -371 50 0 50 0 -2 368 -3 367 -47 3 -48 3 0 -370z"/>
 <path d="M153 699 c-51 -25 -89 -67 -116 -128 -15 -34 -18 -60 -15 -116 4 -91
 40 -155 112 -200 86 -53 227 -46 306 15 l25 19 -29 30 -29 30 -30 -21 c-45
@@ -55,15 +55,3 @@ m120 -6 c3 -9 -3 -11 -20 -7 -31 8 -52 -15 -43 -46 6 -17 12 -20 37 -15 21 4
     </g>
   </svg>
 </template>
-
-<style lang="ts" scoped>
-  .dynamic-color-svg {
-    color: black; /* Default color for the SVG */
-  }
-
-  @media (prefers-color-scheme: dark) {
-    body.dark-mode .dynamic-color-svg {
-      color: white; /* Color for the SVG in dark mode */
-    }
-  }
-</style>
